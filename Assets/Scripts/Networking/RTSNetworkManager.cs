42516f89d6c5bf809a80bc439e505affad679fe4
Ron Welch <mmcelt@yahoo.com>
2020-11-25 13:32:00 -0700

    14 - Unit Movement

---
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -0,0 +1,27 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTSNetworkManager : NetworkManager
+{	
+	#region Fields
+
+	
+	#endregion
+
+	#region MonoBehaviour Methods
+
+
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
---
+++ Assets/Scripts/Units/UnitMovement.cs
@@ -0,0 +1,48 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+public class UnitMovement: NetworkBehaviour
+{
+	#region Fields
+
+	[SerializeField] NavMeshAgent _navAgent;
+
+	Camera _mainCamera;
+
+	#endregion
+
+	#region Server Methods
+
+	[Command]
+	void CmdMove(Vector3 position)
+	{
+		if (!NavMesh.SamplePosition(position, out NavMeshHit hit, 1f, NavMesh.AllAreas)) return;
+		_navAgent.SetDestination(hit.position);
+	}
+	#endregion
+
+	#region Client Methods
+
+	public override void OnStartAuthority()
+	{
+		_mainCamera = Camera.main;
+	}
+
+	[ClientCallback]	//prevents the server from running this code
+	void Update()
+	{
+		if (!hasAuthority) return;	//if not the local player, return
+		if (!Mouse.current.rightButton.wasPressedThisFrame) return;
+
+		Ray ray = _mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+		if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity)) return;
+
+		CmdMove(hit.point);
+	}
+	#endregion
+}
