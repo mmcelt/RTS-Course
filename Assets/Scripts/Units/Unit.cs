30e1801748a313e787000b24642f3d2cb3250790
Ron Welch <mmcelt@yahoo.com>
2020-11-25 18:06:39 -0700

    16 - Selecting a Unit

---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,52 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Unit : NetworkBehaviour
+{
+	#region Fields
+
+	[SerializeField] UnityEvent OnSelected;
+	[SerializeField] UnityEvent OnDeselected;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		
+	}
+	#endregion
+
+	#region Public Client Methods
+
+	[Client]
+	public void Select()
+	{
+		if (!hasAuthority) return;
+
+		OnSelected?.Invoke();
+	}
+
+	[Client]
+	public void Deselect()
+	{
+		if (!hasAuthority) return;
+
+		OnDeselected?.Invoke();
+	}
+	#endregion
+
+	#region Private Client Methods
+
+
+	#endregion
+}
---
+++ Assets/Scripts/Units/UnitSelectionHandler.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UnitSelectionHandler: MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] LayerMask layerMask = new LayerMask();
+
+	Camera _mainCamera;
+
+	List<Unit> _selectedUnits = new List<Unit>();
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		_mainCamera = Camera.main;
+	}
+	
+	void Update() 
+	{
+		if (Mouse.current.leftButton.wasPressedThisFrame)
+		{
+			//start the selection area...
+			foreach(Unit selectedUnit in _selectedUnits)
+			{
+				selectedUnit.Deselect();
+			}
+			_selectedUnits.Clear();
+		}
+		else if (Mouse.current.leftButton.wasReleasedThisFrame)
+		{
+			ClearSelectionArea();
+		}
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+	void ClearSelectionArea()
+	{
+		Ray ray = _mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+		if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask)) return;
+
+		if (!hit.collider.TryGetComponent<Unit>(out Unit unit)) return;
+
+		if (!unit.hasAuthority) return;
+
+		_selectedUnits.Add(unit);
+		foreach(Unit selectedUnit in _selectedUnits)
+		{
+			selectedUnit.Select();
+		}
+	}
+	#endregion
+}
