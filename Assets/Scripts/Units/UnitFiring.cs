923107338c6b881a8f89e7f59f2dc66f998cd7eb
Ron Welch <mmcelt@yahoo.com>
2020-11-28 14:03:36 -0700

    23 - Unit Firing

---
+++ Assets/Scripts/Units/UnitFiring.cs
@@ -0,0 +1,59 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitFiring : NetworkBehaviour
+{
+	#region Fields
+
+	[SerializeField] Targeter _targeter;
+	[SerializeField] GameObject _projectilePrefab;
+	[SerializeField] Transform _firePoint;
+	[SerializeField] float _fireRange = 5f, _fireRate = 1f, _rotationSpeed = 20f;
+
+	float _lastFireTime;
+
+	#endregion
+
+	#region Server Methods
+
+	[ServerCallback]
+	void Update()
+	{
+		Targetable target = _targeter.GetTarget();
+
+		if ( target == null) return;
+		if (!CanFireAtTarget()) return;
+
+		Quaternion targetRotation = Quaternion.LookRotation(target.transform.position - transform.position);
+
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+
+		if(Time.time > (1 / _fireRate) + _lastFireTime)
+		{
+			//we can fire now...
+
+			Quaternion projectileRotation = Quaternion.LookRotation(target.GetAimPoint().position - _firePoint.position);
+
+			GameObject projectileGO = Instantiate(_projectilePrefab, _firePoint.position, projectileRotation);
+
+			NetworkServer.Spawn(projectileGO, connectionToClient);
+
+			_lastFireTime = Time.time;
+		}
+	}
+
+	[Server]
+	bool CanFireAtTarget()
+	{
+		//distance check...
+		return (_targeter.GetTarget().transform.position - transform.position).sqrMagnitude <= _fireRange * _fireRange;
+	}
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
---
+++ Assets/Scripts/Units/UnitProjectile.cs
@@ -0,0 +1,42 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitProjectile : NetworkBehaviour
+{
+	#region Fields
+
+	[SerializeField] Rigidbody _rb;
+	[SerializeField] float _launchForce = 10f;
+	[SerializeField] float _lifetime = 5f;
+
+	#endregion
+
+	#region NetworkBehaviour Methods
+
+	void Start() 
+	{
+		_rb.velocity = transform.forward * _launchForce;
+	}
+
+	public override void OnStartServer()
+	{
+		Invoke(nameof(SelfDestruct), _lifetime);
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+	[Server]
+	void SelfDestruct()
+	{
+		NetworkServer.Destroy(gameObject);
+	}
+	#endregion
+}
