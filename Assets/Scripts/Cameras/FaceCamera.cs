5760d238bee5820b9e3f1a075bf6920e6adffe8a
Ron Welch <mmcelt@yahoo.com>
2020-12-05 16:35:30 -0700

    30 - Buildings Setup

---
+++ Assets/Scripts/Cameras/FaceCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+	#region Fields
+
+	Transform _mainCameraTransform;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		_mainCameraTransform = Camera.main.transform;
+	}
+	
+	void LateUpdate() 
+	{
+		transform.LookAt(transform.position + _mainCameraTransform.rotation * Vector3.forward, _mainCameraTransform.rotation * Vector3.up);
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
